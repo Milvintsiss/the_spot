5a6ef43316b9799502739c69d7e6222cd26fa37b
Milvintsiss <Milvintsiss@users.noreply.github.com>
2019-12-05 20:49:49 +0100

    Initial commit

---
+++ lib/pages/inscription_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class InscriptionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
