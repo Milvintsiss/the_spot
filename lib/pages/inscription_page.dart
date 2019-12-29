3dc42c71f17c9992d8531d2d58f6d98a7dd4261d
Paulo1026 <Paulo1026@users.noreply.github.com>
2019-12-29 02:38:34 +0100

    add translations

--- lib/pages/inscription_page.dart
+++ lib/pages/inscription_page.dart
@@ -1,17 +1,84 @@
 import 'package:flutter/material.dart';
 
+import 'package:the_spot/app_localizations.dart';
 
-class InscriptionPage extends StatelessWidget {
+class InscriptionPage extends StatefulWidget {
+  const InscriptionPage({Key key}) : super(key: key);
+
+  @override
+  _InscriptionPage createState() => _InscriptionPage();
+}
+
+class _InscriptionPage extends State<InscriptionPage> {
+  String _pseudo;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
+    return SafeArea(
+        child: Scaffold(
+            backgroundColor: Colors.blueGrey[700],
+            body:
+
+
+          Padding(
+              padding: EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 0.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  showPseudoInput(),
+                  showPrimaryButton(),
+                ],
+              ),
+            )));
+  }
+
+  Widget showPseudoInput() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+      child: new TextFormField(
+        style: TextStyle(color: Colors.white),
+        maxLines: 1,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: AppLocalizations.of(context).translate('Username'),
+            hintStyle: TextStyle(color: Colors.blueGrey[100]),
+            fillColor: Colors.blueGrey[500],
+            filled: true,
+            contentPadding: new EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+            border: new OutlineInputBorder(
+              borderRadius: new BorderRadius.circular(12.0),
+            ),
+            ),
+        validator: (value) => value.isEmpty ? 'Pseudo can\'t be empty' : null,
+        onSaved: (value) => _pseudo = value.trim(),
+      ),
+    );
+  }
+
+  Widget showPrimaryButton() {
+    return new Padding(
+      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+      child: SizedBox(
+        height: 40.0,
+        child: new RaisedButton(
+          elevation: 5.0,
+          shape: new RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(30.0)),
+          color: Colors.blueGrey[500],
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Text('Next',
+                style: new TextStyle(fontSize: 15.0, color: Colors.white),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                child: Icon(Icons.arrow_forward, color: Colors.white, size: 20,),
+              )
+            ],
+          ),
+          onPressed: () => 'next',
         ),
       ),
     );
