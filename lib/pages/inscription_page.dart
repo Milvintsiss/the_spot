850bd07c3a761c3688c02cef93524e36efb72d47
Paulo1026 <Paulo1026@users.noreply.github.com>
2020-02-10 01:49:50 +0100

    add avatar widget for inscription_page
    To add:
    -Cloud storage for avatars

--- lib/pages/inscription_page.dart
+++ lib/pages/inscription_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'package:the_spot/app_localizations.dart';
 import 'package:the_spot/pages/main.dart';
@@ -23,41 +24,67 @@ class _InscriptionPage extends State<InscriptionPage> {
     return SafeArea(
         child: Scaffold(
             backgroundColor: PrimaryColorDark,
-            body:
-
-
-          Padding(
+            body: ListView(
               padding: EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  showPseudoInput(),
-                  showPracticesButtons(),
-                  showNextButton(),
-                ],
-              ),
+              children: <Widget>[
+                showAvatarWidget(),
+                showPseudoInput(),
+                showPracticesButtons(),
+                showNextButton(),
+              ],
             )));
+  }
+
+  Widget showAvatarWidget() {
+    return Padding(
+        padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+        child: GestureDetector(
+          onTap: () => print("add an Avatar"),
+          child: CircleAvatar(
+            backgroundColor: PrimaryColorLight,
+            radius: 80,
+            foregroundColor: PrimaryColorDark,
+            child: Stack(
+              overflow: Overflow.visible,
+                children: <Widget>[
+              Icon(
+                Icons.person,
+                size: 100,
+              ),
+              Positioned(
+                bottom: -40,
+                  right: -40,
+                  child: Icon(
+                    Icons.add_circle,
+                    size: 60,
+                    color: SecondaryColor,
+                  )
+              ),
+            ]),
+          ),
+        ));
   }
 
   Widget showPseudoInput() {
     return Padding(
-      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+      padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
       child: new TextFormField(
         style: TextStyle(color: Colors.white),
         maxLines: 1,
         autofocus: false,
         decoration: new InputDecoration(
-            hintText: AppLocalizations.of(context).translate('Username'),
-            hintStyle: TextStyle(color: Colors.blueGrey[100]),
-            fillColor: PrimaryColorLight,
-            filled: true,
-            contentPadding: new EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-            border: new OutlineInputBorder(
-              borderRadius: new BorderRadius.circular(12.0),
-            ),
-            ),
-        validator: (value) => value.isEmpty ? AppLocalizations.of(context).translate('Pseudo can\'t be empty') : null,
+          hintText: AppLocalizations.of(context).translate('Username'),
+          hintStyle: TextStyle(color: Colors.blueGrey[100]),
+          fillColor: PrimaryColorLight,
+          filled: true,
+          contentPadding: new EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+          border: new OutlineInputBorder(
+            borderRadius: new BorderRadius.circular(12.0),
+          ),
+        ),
+        validator: (value) => value.isEmpty
+            ? AppLocalizations.of(context).translate('Pseudo can\'t be empty')
+            : null,
         onSaved: (value) => _pseudo = value.trim(),
       ),
     );
@@ -76,11 +103,16 @@ class _InscriptionPage extends State<InscriptionPage> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Material(
-                color: PrimaryColorDark,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  color: PrimaryColorDark,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Text("Select your hobby(ies)", style: TextStyle(color: PrimaryColorLight, fontSize: 20)),
+                    child: Text(
+                        AppLocalizations.of(context)
+                            .translate('Select your hobby(ies)'),
+                        style: TextStyle(
+                            color: PrimaryColorLight, fontSize: 17.5)),
                   )),
             ),
             Container(
@@ -106,26 +138,31 @@ class _InscriptionPage extends State<InscriptionPage> {
       ),
     );
   }
-  Widget practiceButton (String practice){
+
+  Widget practiceButton(String practice) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Material(
-        // needed
-        shape: RoundedRectangleBorder(
-          borderRadius: new BorderRadius.circular(14.0),
-          side: BorderSide(width: 3, color: practice == "assets/images/Roller.png" && _Roller
-              || practice == "assets/images/BMX.png" && _BMX
-              || practice == "assets/images/Skateboard.png" && _Skateboard
-              || practice == "assets/images/Scooter.png" && _Scooter
-              ? Colors.white : PrimaryColorDark)
-        ),
-        color: PrimaryColor,
+      child: GestureDetector(
+        onTap: () => onTapPractices(practice),
+        child: Material(
+          // needed
+          shape: RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(14.0),
+              side: BorderSide(
+                  width: 3,
+                  color: practice == "assets/images/Roller.png" && _Roller ||
+                          practice == "assets/images/BMX.png" && _BMX ||
+                          practice == "assets/images/Skateboard.png" &&
+                              _Skateboard ||
+                          practice == "assets/images/Scooter.png" && _Scooter
+                      ? Colors.white
+                      : PrimaryColorDark)),
+          color: PrimaryColor,
 
-        child: GestureDetector(
-          onTap: () => onTapPractices(practice),
           child: Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Image.asset(practice,
+            child: Image.asset(
+              practice,
               width: 45,
               fit: BoxFit.cover,
             ),
@@ -134,42 +171,51 @@ class _InscriptionPage extends State<InscriptionPage> {
       ),
     );
   }
-  void onTapPractices(String practice){
+
+  void onTapPractices(String practice) {
     switch (practice) {
-      case "assets/images/Roller.png" : _Roller = !_Roller;
-      break;
-      case "assets/images/BMX.png" : _BMX = !_BMX;
-      break;
-      case "assets/images/Skateboard.png" : _Skateboard = !_Skateboard;
-      break;
-      case "assets/images/Scooter.png" : _Scooter = !_Scooter;
-      break;
+      case "assets/images/Roller.png":
+        _Roller = !_Roller;
+        break;
+      case "assets/images/BMX.png":
+        _BMX = !_BMX;
+        break;
+      case "assets/images/Skateboard.png":
+        _Skateboard = !_Skateboard;
+        break;
+      case "assets/images/Scooter.png":
+        _Scooter = !_Scooter;
+        break;
     }
     setState(() {
       practiceButton(practice);
     });
-
   }
 
   Widget showNextButton() {
     return new Padding(
-      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+      padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
       child: SizedBox(
         height: 40.0,
         child: new RaisedButton(
           elevation: 5.0,
           shape: new RoundedRectangleBorder(
               borderRadius: new BorderRadius.circular(30.0)),
-          color: SecondaryColorDark,
+          color: SecondaryColor,
           child: Row(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              new Text(AppLocalizations.of(context).translate('Next'),
+              new Text(
+                AppLocalizations.of(context).translate('Next'),
                 style: new TextStyle(fontSize: 15.0, color: Colors.white),
               ),
               Padding(
                 padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
-                child: Icon(Icons.arrow_forward, color: Colors.white, size: 20,),
+                child: Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                  size: 20,
+                ),
               )
             ],
           ),
