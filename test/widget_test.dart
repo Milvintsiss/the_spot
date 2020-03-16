301f80f85032099a2123fa1aa93730783d7c5aa8
Paulo1026 <Paulo1026@users.noreply.github.com>
2020-03-16 20:35:48 +0100

    change main.dart location
    To add:
    -Verification if username already used
    -start the homePage (bottomNavigationBar and Map)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:the_spot/pages/main.dart';
+import 'file:///C:/Users/plest/StudioProjects/the_spot/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
