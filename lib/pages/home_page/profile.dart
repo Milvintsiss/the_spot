24e288dcaffdaa8f7a54c012b551794f62a42944
Paulo1026 <Paulo1026@users.noreply.github.com>
2020-05-03 19:29:46 +0200

    new: improve profile page UI
    To add:
    repair users follow/unFollow function
    improve friend/follow system
    improve userLocation system
    show all users positions on the map
    tags for spots
    
    Firebase function to suppress all spots who haven't been updated
    
    Firebase function to suppress all photos isn't referenced in the database
    
    Firebase function to suppress all residual data when suppress user fail

--- lib/pages/home_page/profile.dart
+++ lib/pages/home_page/profile.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
@@ -33,6 +34,7 @@ class Profile extends StatefulWidget {
 
 class _Profile extends State<Profile> {
   bool isUser;
+  bool isInstaClipSelected = false;
 
   @override
   void initState() {
@@ -53,38 +55,59 @@ class _Profile extends State<Profile> {
     }
   }
 
+  void uploadAvatar() async {
+    print("add an Avatar");
+    await Storage().getPhotoFromUserStorageAndUpload(
+      storageRef: "ProfilePictures/" + widget.userProfile.userId,
+      context: context,
+      cropStyle: CropStyle.circle,
+      cropAspectRatio: CropAspectRatio(ratioX: 1.0, ratioY: 1.0),
+      maxHeight: 150,
+      maxWidth: 150,
+      compressQuality: 75,
+    );
+
+    String profilePictureDownloadPath = await Storage()
+        .getUrlPhoto("ProfilePictures/" + widget.userProfile.userId);
+
+    await Database().updateProfile(context, widget.userProfile.userId,
+        profilePictureDownloadPath: profilePictureDownloadPath);
+
+    setState(() {
+      widget.userProfile.profilePictureDownloadPath =
+          profilePictureDownloadPath;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: PrimaryColorDark,
       appBar: showAppBar(),
       endDrawer: showDrawer(),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 30, 16, 0),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    showAvatarWidget(),
-                    showProfileWidget(),
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],
+      body: Padding(
+        padding: EdgeInsets.only(top: widget.configuration.screenWidth / 20),
+        child: Column(
+          children: <Widget>[
+            showTopProfile(),
+            showNUmberOfFriendsFollowersFollowing(),
+            showSelectorButton(),
+            showVideosLayout(),
+          ],
+        ),
       ),
     );
   }
 
   AppBar showAppBar() {
     if (isUser)
-      return AppBar(actions: <Widget>[
+      return AppBar(
+          backgroundColor: PrimaryColorDark,
+          actions: <Widget>[
         Builder(builder: (BuildContext context) {
           return IconButton(
-            icon: Icon(Icons.settings),
+            icon: Hero(
+                tag: 'optionButton_profilePage', child: Icon(Icons.settings)),
             onPressed: () => Scaffold.of(context).openEndDrawer(),
             tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
           );
@@ -92,6 +115,7 @@ class _Profile extends State<Profile> {
       ]);
     else
       return AppBar(
+        backgroundColor: PrimaryColorDark,
         leading: IconButton(
           icon: Icon(
             Icons.arrow_back,
@@ -128,10 +152,6 @@ class _Profile extends State<Profile> {
       color: PrimaryColor,
       height: 55,
       child: ListTile(
-        title: Text(
-          'Settings',
-          style: TextStyle(fontSize: 28, color: SecondaryColorDark),
-        ),
         leading: Icon(
           Icons.settings,
           size: 40,
@@ -162,11 +182,12 @@ class _Profile extends State<Profile> {
               case 'Edit my Profile':
                 {
                   Navigator.pop(context);
-                  await Navigator.push(context, MaterialPageRoute(
-                    builder: (context) => InscriptionPage(
-                      configuration: widget.configuration,
-                    )
-                  ));
+                  await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => InscriptionPage(
+                                configuration: widget.configuration,
+                              )));
                   setState(() {});
                 }
                 break;
@@ -193,106 +214,232 @@ class _Profile extends State<Profile> {
     );
   }
 
+  Widget showTopProfile() {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [PrimaryColorDark, PrimaryColor]
+        )
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            padding:
+                EdgeInsets.fromLTRB(widget.configuration.screenWidth / 40, 0, widget.configuration.screenWidth / 30, 0),
+            width: widget.configuration.screenWidth / 3,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                showPseudoWidget(),
+                showUsernameWidget(),
+              ],
+            ),
+          ),
+          showAvatarWidget(),
+          Container(
+            width: widget.configuration.screenWidth / 3,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    practiceButton("assets/images/BMX.png"),
+                    practiceButton("assets/images/Skateboard.png"),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    practiceButton("assets/images/Scooter.png"),
+                    practiceButton("assets/images/Roller.png"),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget showAvatarWidget() {
     return Hero(
       tag: widget.userProfile.userId,
       child: GestureDetector(
         onTap: isUser ? uploadAvatar : null,
-        child: Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-            child: Stack(overflow: Overflow.visible, children: <Widget>[
-              ProfilePicture(widget.userProfile.profilePictureDownloadPath,
-                  size: 180, borderColor: PrimaryColor),
-              isUser
-                  ? Positioned(
-                      bottom: -7.5,
-                      right: -7.5,
-                      child: Icon(
-                        Icons.add_circle,
-                        size: 60,
-                        color: SecondaryColor,
-                      ))
-                  : Container(),
-            ])),
+        child: Stack(overflow: Overflow.visible, children: <Widget>[
+          ProfilePicture(widget.userProfile.profilePictureDownloadPath,
+              size: widget.configuration.screenWidth / 3,
+              borderColor: PrimaryColor),
+          isUser
+              ? Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Icon(
+                    Icons.add_circle,
+                    size: 40,
+                    color: SecondaryColor,
+                  ))
+              : Container(),
+        ]),
       ),
     );
   }
 
-  Widget showProfileWidget() {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
-      child: Container(
-        padding: EdgeInsets.all(12),
-        decoration: BoxDecoration(
+  Widget showPseudoWidget() {
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      child: Text(widget.userProfile.pseudo,
+          style: TextStyle(
             color: PrimaryColorLight,
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            border: Border.fromBorderSide(
-                BorderSide(color: PrimaryColor, width: 3))),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+            fontSize: 30 * widget.configuration.textSizeFactor,
+          )),
+    );
+  }
+
+  Widget showUsernameWidget() {
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      child: Text('@' + widget.userProfile.username,
+          style: TextStyle(color: SecondaryColorDark,
+            fontSize: 16 * widget.configuration.textSizeFactor,)),
+    );
+  }
+
+  Widget practiceButton(String practice) {
+    bool isSelected = practice == "assets/images/Roller.png" &&
+        widget.userProfile.Roller ||
+        practice == "assets/images/BMX.png" && widget.userProfile.BMX ||
+        practice == "assets/images/Skateboard.png" &&
+            widget.userProfile.Skateboard ||
+        practice == "assets/images/Scooter.png" &&
+            widget.userProfile.Scooter;
+    return Padding(
+      padding: const EdgeInsets.all(2),
+      child: Material(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(14.0),
+        ),
+        color: isSelected
+            ? PrimaryColorLight
+            : transparentColor(PrimaryColorLight, 40),
+        child: Padding(
+          padding: EdgeInsets.all(widget.configuration.screenWidth / 45),
+          child: Image.asset(
+            practice,
+            color: isSelected ? Colors.black : transparentColor(Colors.black, 40),
+            width: widget.configuration.screenWidth / 15,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget showNUmberOfFriendsFollowersFollowing() {
+    return Container(
+      color: PrimaryColor,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0, widget.configuration.screenWidth / 30, 0, widget.configuration.screenWidth / 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            showPseudoWidget(),
-            showUsernameWidget(),
+            Container(
+              width: widget.configuration.screenWidth / 3,
+              child: Center(
+                child: Text('Friends: ' + widget.userProfile.numberOfFriends.toString(),
+                style: TextStyle(color: SecondaryColor, fontSize: 16 * widget.configuration.textSizeFactor),),
+              ),
+            ),
+            Container(
+              width: widget.configuration.screenWidth / 3,
+              child: Center(
+                child: Text('Followers: ' + widget.userProfile.numberOfFollowers.toString(),
+                  style: TextStyle(color: SecondaryColor, fontSize: 16 * widget.configuration.textSizeFactor),),
+              ),
+            ),
+            Container(
+              width: widget.configuration.screenWidth / 3,
+              child: Center(
+                child: Text('Following: ' + widget.userProfile.numberOfFollowing.toString(),
+                  style: TextStyle(color: SecondaryColor, fontSize: 16 * widget.configuration.textSizeFactor),),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget showPseudoWidget() {
-    return RichText(
-      text: TextSpan(
-          style:
-              TextStyle(color: PrimaryColorDark, fontWeight: FontWeight.bold),
-          children: <TextSpan>[
-            TextSpan(
-              text: 'Pseudo: ',
+  Widget showSelectorButton() {
+    return Row(
+      children: <Widget>[
+        InkWell(
+          onTap: () => setState(() {
+            isInstaClipSelected = false;
+          }),
+          child: Container(
+            width: widget.configuration.screenWidth / 2,
+            decoration: BoxDecoration(
+              color: isInstaClipSelected ? PrimaryColor : PrimaryColorLight,
+              border: Border.all(width: 2, color: isInstaClipSelected ? PrimaryColor : PrimaryColorDark),
             ),
-            TextSpan(
-                text: widget.userProfile.pseudo,
-                style: TextStyle(color: SecondaryColor))
-          ]),
+            padding: EdgeInsets.all(widget.configuration.screenWidth / 40),
+            child: Icon(
+              Icons.videocam,
+              size: widget.configuration.screenWidth / 20,
+            ),
+          ),
+        ),
+        InkWell(
+          onTap: () => setState(() {
+            isInstaClipSelected = true;
+          }),
+          child: Container(
+            width: widget.configuration.screenWidth / 2,
+            decoration: BoxDecoration(
+              color: !isInstaClipSelected ? PrimaryColor : PrimaryColorLight,
+              border: Border.all(width: 2, color: !isInstaClipSelected ? PrimaryColor : PrimaryColorDark),
+            ),
+            padding: EdgeInsets.all(widget.configuration.screenWidth / 40),
+            child: Icon(
+              Icons.phone_android,
+              size: widget.configuration.screenWidth / 20,
+            ),
+          ),
+        ),
+      ],
     );
   }
 
-  Widget showUsernameWidget() {
-    return RichText(
-      text: TextSpan(
-          style: TextStyle(color: PrimaryColorDark),
-          children: <TextSpan>[
-            TextSpan(
-                text: 'Username: ',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                )),
-            TextSpan(
-                text: "@" + widget.userProfile.username,
-                style: TextStyle(fontStyle: FontStyle.italic))
-          ]),
+  Widget showVideosLayout() {
+    bool userHasVids = false;
+    return Expanded(
+      child: Container(
+        color: PrimaryColorLight,
+        child: userHasVids ? ListView(
+          children: <Widget>[],
+        ) :
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Icon(
+                    Icons.priority_high,
+                    size: widget.configuration.screenWidth / 6,
+                  ),
+                ),
+                Divider(height: widget.configuration.screenWidth / 20),
+                Text('This user hasn\'t posted any vid for the moment', style: TextStyle(color: Colors.black, fontSize: 18 * widget.configuration.textSizeFactor),)
+              ],
+            )
+      ),
     );
-  }
-
-  void uploadAvatar() async {
-    print("add an Avatar");
-    await Storage().getPhotoFromUserStorageAndUpload(
-      storageRef: "ProfilePictures/" + widget.userProfile.userId,
-      context: context,
-      cropStyle: CropStyle.circle,
-      cropAspectRatio: CropAspectRatio(ratioX: 1.0, ratioY: 1.0),
-      maxHeight: 150,
-      maxWidth: 150,
-      compressQuality: 75,
-    );
-
-    String profilePictureDownloadPath = await Storage()
-        .getUrlPhoto("ProfilePictures/" + widget.userProfile.userId);
-
-    await Database().updateProfile(context, widget.userProfile.userId,
-        profilePictureDownloadPath: profilePictureDownloadPath);
-
-    setState(() {
-      widget.userProfile.profilePictureDownloadPath =
-          profilePictureDownloadPath;
-    });
   }
 }
